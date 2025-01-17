416524ab0916f591de7082f710075eb3554332ca
Amin Farzanepey <aminfarzanepey@gmail.com>
2025-01-17 11:43:46 +0330

    adding GetWeather function and api key

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ subBtn = document.getElementById("submitBtn");
 cityName = document.getElementById("cityName");
 cityDisc = document.getElementById("cityDicript");
 degree = document.getElementById("degree");
+wind = document.getElementById("wind");
+
+const apiKey = "3045dd712ffe6e702e3245525ac7fa38";
+
+async function GetWeather() {
+    var weatherResult = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}`)).json();
+}
+
+
+
+subBtn.addEventListener("click", GetWeather);
