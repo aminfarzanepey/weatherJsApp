209308a6cc735a8ea54ee5ffb950eec029355c65
Amin Farzanepey <aminfarzanepey@gmail.com>
2025-01-17 11:22:22 +0330

    adding files and modify css and html

---
+++ script.js
@@ -0,0 +1,5 @@
+cityInput = document.getElementById("inputCity");
+subBtn = document.getElementById("submitBtn");
+cityName = document.getElementById("cityName");
+cityDisc = document.getElementById("cityDicript");
+degree = document.getElementById("degree");
